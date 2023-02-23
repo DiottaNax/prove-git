de0f90163013d9ee468187345c957e57628596ce
Nax <federico.diotallevi@studio.unibo.it>
2023-02-23 19:44:16 +0100

    modified Main to greed Diotta

--- src/Main.java
+++ src/Main.java
@@ -9,5 +9,6 @@ public class Main {
     public static void main(String... args) {
         hi = new HelloToImpl("Nax");
         ((HelloToImpl) hi).greed();
+        ((HelloToImpl) hi).greed("Diotta");
     }
 }
